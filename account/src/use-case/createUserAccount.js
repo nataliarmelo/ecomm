19c022cf9977e3746abf2bb758aede129f942686
MELO NATALIA RODRIGUES DE <n851187@gruposantander.com>
2022-12-16 09:36:43 -0300

    Criando usuário via POST

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,13 +1,13 @@
 import { saveAccount } from '../../repositories/accountRepository.js'; //importar a função saveAccount de Repositories.
 
-export function createUserUseCase(nome, email, senha){
+export function createUserUseCase(name, email, password){
     
     const createdDate = new Date().toISOString().substring(0,10);
 
     const user = {
-         nome,
+         name,
          email,
-         senha,
+         password,
          createdDate
     };
     
