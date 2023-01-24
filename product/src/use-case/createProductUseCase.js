f4c668e9cbfe6ace1c0e3d9b328569c46e9ea0a4
NATALIA RODRIGUES DE MELO <nahrdmelo@gmail.com>
2023-01-24 19:25:55 -0300

    authorization rules

--- product/src/use-case/createProductUseCase.js
+++ product/src/use-case/createProductUseCase.js
@@ -1,10 +1,8 @@
-import { saveProduct } from '../repositories/productRepository.js'; 
-// importante importar o saveProduct para conseguirmos salvar o produto dentro
-
-//essa função irá criar o produto e suas caracteristicas
-export async function createProductUseCase(product) {
-    const savedProduct = await saveProduct(product);
-    return savedProduct;
-};
+import { saveProduct } from "../repositories/productRepository.js";
 
 
+
+export async function createProductUseCase(product, userId) {
+  const savedProduct = await saveProduct({ ...product, user_id: userId });
+  return savedProduct;
+}
