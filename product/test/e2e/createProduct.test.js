76282b2f236a275f789052e6a892a59d8e5f4bde
NATALIA RODRIGUES DE MELO <nahrdmelo@gmail.com>
2023-02-03 19:43:37 -0300

    validation to product

--- product/test/e2e/createProduct.test.js
+++ product/test/e2e/createProduct.test.js
@@ -1,4 +1,5 @@
 import request from "supertest";
+import { randomUUID } from "crypto";
 import { app } from "../../src/app.js";
 import { product } from "../data/products.js";
 import { cleanProductTable } from "../helpers/help-product.js";
@@ -10,17 +11,21 @@ describe("Product Creation", () => {
   });
 
   it("Should create a product", async () => {
+    const userId = randomUUID();
+    const userToken = generateToken(userId, 20);
+
     await request(app)
       .post("/products")
       .set("Content-Type", "application/json")
       .set("Accept", "application/json")
-      .set("Authorization", `Bearer ${generateToken("id-do-usuario")}`)
+      .set("Authorization", `Bearer ${userToken}`)
       .send(product)
       .expect(201)
       .expect(({ body }) => {
         expect(body).toEqual({
           ...product,
           id: expect.any(Number),
+          user_id: userId, // quando insiro o user_id da erro de invalid signature - o teste passava sem o user_id.
           createdAt: expect.any(String),
           updatedAt: expect.any(String),
           features: product.features.map((feature) => ({
@@ -39,7 +44,7 @@ describe("Product Creation", () => {
           })),
         });
       });
-  });  
+  });
   it("should not create a product when no authorization info is provided", async () => {
     await request(app)
       .post("/products")
@@ -64,7 +69,7 @@ describe("Product Creation", () => {
   });
 
   it("should not create a product when authorization info was modified", async () => {
-    const modifiedToken = generateToken("id-usuario") + "a";
+    const modifiedToken = generateToken(randomUUID()) + "a";
     await request(app)
       .post("/products")
       .set("Content-Type", "application/json")
@@ -73,6 +78,19 @@ describe("Product Creation", () => {
       .expect(403)
       .expect(({ body }) => {
         expect(body).toEqual({ message: "forbidden" });
+      });
+  });
+
+  it("should not create a product when user id is not valid", async () => {
+    await request(app)
+      .post("/products")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .set("Authorization", `Bearer ${generateToken(randomUUID())}`)
+      .send({ ...product })
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body).toEqual({ message: "user id is not valid" });
       });
   });
 });
